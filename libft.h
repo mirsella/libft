b27b1e278ebd73ded30619dc0e2079cbb032ec70
mirsella <mirsella@protonmail.com>
2022-11-22 18:48:19 +0100

    add putnbr_base

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mirsella <mirsella@protonmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 22:35:33 by mirsella          #+#    #+#             */
-/*   Updated: 2022/11/22 18:35:41 by mirsella         ###   ########.fr       */
+/*   Updated: 2022/11/22 18:40:26 by mirsella         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,6 +39,7 @@ int		ft_toupper(int c);
 int		ft_putchar(char c);
 int		ft_putstr(char *s);
 int		ft_putnbr(int n);
+int		ft_putnbr_base(int n, char *base);
 int		ft_max(int a, int b);
 int		ft_min(int a, int b);
 int		ft_nbrlen(int n);
