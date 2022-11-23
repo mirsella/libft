62458d6786e2162c0ebdc58118111573b2cc46d6
mirsella <mirsella@protonmail.com>
2022-11-23 16:04:51 +0100

    add ft_ullmax/ullmin

--- ft_nbrlen.c
+++ ft_nbrlen.c
@@ -6,7 +6,7 @@
 /*   By: mirsella <mirsella@protonmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/22 18:31:57 by mirsella          #+#    #+#             */
-/*   Updated: 2022/11/23 15:31:54 by mirsella         ###   ########.fr       */
+/*   Updated: 2022/11/23 15:54:37 by mirsella         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,10 @@ int	ft_nbrlen(long long n)
 	int	len;
 
 	len = 0;
-	if (n == 0)
-		return (1);
 	if (n == -9223372036854775807 - 1)
 		return (20);
+	if (n == 0)
+		return (1);
 	if (n < 0)
 	{
 		n = -n;
@@ -46,6 +46,36 @@ int	ft_nbrlen_base(long long n, int base)
 		n = -n;
 		len++;
 	}
+	while (n > 0)
+	{
+		n /= base;
+		len++;
+	}
+	return (len);
+}
+
+int	ft_ullnbrlen(unsigned long long n)
+{
+	int	len;
+
+	len = 0;
+	if (n == 0)
+		return (1);
+	while (n > 0)
+	{
+		n /= 10;
+		len++;
+	}
+	return (len);
+}
+
+int	ft_ullnbrlen_base(unsigned long long n, int base)
+{
+	int	len;
+
+	len = 0;
+	if (n == 0)
+		return (1);
 	while (n > 0)
 	{
 		n /= base;
