bf7f2f839aeced9c3548ba4848f9c3d927b06784
mirsella <mirsella@protonmail.com>
2023-01-02 15:45:48 +0100

    add some lst functions

---
+++ ft_lstfind.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstfind.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mirsella <mirsella@protonmail.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/02 15:35:08 by mirsella          #+#    #+#             */
+/*   Updated: 2023/01/02 15:38:30 by mirsella         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_list_find(t_list *begin_list, void *data_ref, int (*cmp)(void *, void *))
+{
+	t_list	*list;
+
+	list = begin_list;
+	while (list)
+	{
+		if (cmp(list->content, data_ref) == 0)
+			return (list);
+		list = list->next;
+	}
+	return (0);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mirsella <mirsella@protonmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 22:35:33 by mirsella          #+#    #+#             */
-/*   Updated: 2022/12/26 15:17:42 by mirsella         ###   ########.fr       */
+/*   Updated: 2023/01/02 15:41:20 by mirsella         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,7 +70,10 @@ size_t				ft_strlen(const char *s);
 t_list				*ft_lstlast(t_list *lst);
 t_list				*ft_lstmap(t_list *lst, void *(*f)(void *),
 						void (*del)(void *));
+t_list				*ft_list_find(t_list *begin_list, void *data_ref,
+						int (*cmp)(void *, void *));
 t_list				*ft_lstnew(void *content);
+t_list				*ft_list_at(t_list *begin_list, unsigned int nbr);
 void				*ft_calloc(size_t count, size_t size);
 void				*ft_memccpy(void *dest, const void *src, int c, size_t n);
 void				*ft_memchr(const void *s, int c, size_t n);
