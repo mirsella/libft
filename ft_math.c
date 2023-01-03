5e0c47cd6fcf806e5c3fe94fedf80dd358253cbc
mirsella <mirsella@protonmail.com>
2023-01-03 14:33:13 +0100

    fix norm

--- ft_math.c
+++ ft_math.c
@@ -6,7 +6,7 @@
 /*   By: mirsella <mirsella@protonmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/03 00:36:30 by mirsella          #+#    #+#             */
-/*   Updated: 2023/01/03 11:44:39 by mirsella         ###   ########.fr       */
+/*   Updated: 2023/01/03 14:33:07 by mirsella         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,14 +31,14 @@ int	ft_ceil(double x)
 	return (x + 1);
 }
 
-int     ft_abs(int n)
+int	ft_abs(int n)
 {
 	if (n < 0)
 		return (-n);
 	return (n);
 }
 
-long long       ft_llabs(long long n)
+long long	ft_llabs(long long n)
 {
 	if (n < 0)
 		return (-n);
