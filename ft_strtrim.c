ad21f3464df9e4f508292019c4fee08b41f828fd
mirsella <mirsella@protonmail.com>
2023-01-19 14:47:56 +0100

    fix strtrim invalid read size if s = ""

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: mirsella <mirsella@protonmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 23:13:50 by mirsella          #+#    #+#             */
-/*   Updated: 2022/11/13 15:02:53 by mirsella         ###   ########.fr       */
+/*   Updated: 2023/01/19 14:47:09 by mirsella         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,8 @@ char	*ft_strtrim(char const *s1, char const *set)
 		return (NULL);
 	start = 0;
 	end = ft_strlen(s1) - 1;
+	if ((int)end < 0)
+		return (ft_strdup(""));
 	while (s1[start] && ft_strchr(set, s1[start]))
 		start++;
 	while (s1[end] && ft_strchr(set, s1[end]))
@@ -39,11 +41,3 @@ char	*ft_strtrim(char const *s1, char const *set)
 	ft_strlcpy(trimmed, s1 + start, end - start + 1);
 	return (trimmed);
 }
-
-// #include "stdio.h"
-// int	main(void)
-// {
-// 	char	*s = "          ";
-// 	char	*set = "\t \n";
-// 	printf("'%s'\n", ft_strtrim(s, set));
-// }
