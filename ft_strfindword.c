a65fc74447ee15963232b11e93fef577f6fb952b
mirsella <mirsella@protonmail.com>
2023-02-07 21:27:53 +0100

    fix strtabdup, add ft_strfindword

--- ft_strfindword.c
+++ ft_strfindword.c
@@ -1,17 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strfind.c                                       :+:      :+:    :+:   */
+/*   ft_strfindword.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mirsella <mirsella@protonmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/07 21:16:25 by mirsella          #+#    #+#             */
-/*   Updated: 2023/02/07 21:17:05 by mirsella         ###   ########.fr       */
+/*   Updated: 2023/02/07 21:26:45 by mirsella         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strfindword(const char *s, const char *word)
+char	*ft_strfindword(char *s, const char *word)
 {
+	int	i;
+	int	findi;
+
+	if (word[0] == '\0')
+		return (s);
+	i = 0;
+	while (s[i] != '\0')
+	{
+		findi = 0;
+		while (s[i + findi] == word[findi])
+		{
+			if (word[findi + 1] == '\0')
+				return (&s[i]);
+			findi++;
+		}
+		i++;
+	}
+	return (NULL);
 }
