ceefc128c554c7b8dee3507358087bfb84932528
mirsella <mirsella@protonmail.com>
2023-02-09 21:38:37 +0100

    add ft_ in front of skip_spaces

--- ft_isspace.c
+++ ft_isspace.c
@@ -6,7 +6,7 @@
 /*   By: mirsella <mirsella@protonmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/06 17:17:48 by mirsella          #+#    #+#             */
-/*   Updated: 2023/02/09 17:48:41 by mirsella         ###   ########.fr       */
+/*   Updated: 2023/02/09 21:38:28 by mirsella         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@ int	ft_isspace(char c)
 	return ((c >= 9 && c <= 13) || c == 32);
 }
 
-int	skip_spaces(char *str)
+int	ft_skip_spaces(char *str)
 {
 	int	i;
 
